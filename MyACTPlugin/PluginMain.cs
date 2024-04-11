f3c1f756117b964411b34c60055e75b70e77bfca
呆萌 <admin@diemoe.net>
2024-04-11 22:24:21 +0800

    SimplePluginDemo

---
+++ MyACTPlugin/PluginMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using Advanced_Combat_Tracker;
+using MyACTPlugin.Utils;
+
+namespace MyACTPlugin
+{
+    public class PluginMain : IActPluginV1
+    {
+        // ACT界面(插件列表中)的状态标签
+        private Label lblStatus;
+        internal PluginPage configPage;
+
+        public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
+        {
+            Helpers._tabPage = pluginScreenSpace;
+            lblStatus = pluginStatusText;
+            pluginScreenSpace.Text = "插件标题";
+
+            Helpers._plugin = this;
+
+            Helpers._configPage = configPage = new PluginPage();
+            configPage.Dock = DockStyle.Fill;
+            pluginScreenSpace.Controls.Add(configPage);
+
+            lblStatus.Text = "初始化完成";
+        }
+
+        public void DeInitPlugin()
+        {
+            //settings.save();
+            lblStatus.Text = "插件已卸载";
+        }
+
+    }
+}
---
+++ MyACTPlugin/Utils/Helpers.cs
@@ -0,0 +1,123 @@
+﻿using Advanced_Combat_Tracker;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyACTPlugin.Utils
+{
+    internal static class Helpers
+    {
+        internal static string cachePath;
+        private static string settingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\TTS_CN_Plugin.config.xml");
+
+        internal const int MAX_PATH = 127;
+        internal static PluginMain _plugin;
+        internal static PluginPage _configPage;
+        internal static TabPage _tabPage;
+    }
+
+    public static class Extension
+    {
+        /// <summary>Looks for the next occurrence of a sequence in a byte array</summary>
+        /// <param name="array">Array that will be scanned</param>
+        /// <param name="start">Index in the array at which scanning will begin</param>
+        /// <param name="sequence">Sequence the array will be scanned for</param>
+        /// <returns>
+        ///   The index of the next occurrence of the sequence of -1 if not found
+        /// </returns>
+        public static int IndexOf(this byte[] array, int start, byte[] sequence)
+        {
+            int end = array.Length - sequence.Length; // past here no match is possible
+            byte firstByte = sequence[0]; // cached to tell compiler there's no aliasing
+
+            while (start <= end)
+            {
+                // scan for first byte only. compiler-friendly.
+                if (array[start] == firstByte)
+                {
+                    // scan for rest of sequence
+                    for (int offset = 1; ; ++offset)
+                    {
+                        if (offset == sequence.Length)
+                        { // full sequence matched?
+                            return start;
+                        }
+                        else if (array[start + offset] != sequence[offset])
+                        {
+                            break;
+                        }
+                    }
+                }
+                ++start;
+            }
+
+            // end of array reached without match
+            return -1;
+        }
+    }
+    public static class SafeThreadInvoker
+    {
+        public static object SafeInvoke(this Control control, Delegate method)
+        {
+            var asyncResult = control.BeginInvoke(method);
+
+            return SafeWait(control, asyncResult);
+        }
+
+        public static object SafeInvoke(this Control control, Delegate method, object[] args)
+        {
+            var asyncResult = control.BeginInvoke(method, args);
+
+            return SafeWait(control, asyncResult);
+        }
+
+        private static object SafeWait(Control control, IAsyncResult result)
+        {
+            if (result.CompletedSynchronously)
+            {
+                return control.EndInvoke(result);
+            }
+
+            for (var i = 0; i < 50; i++)
+            {
+                if (control.IsDisposed || control.Disposing || !control.IsHandleCreated)
+                {
+                    break;
+                }
+
+                result.AsyncWaitHandle.WaitOne(100);
+                if (result.IsCompleted)
+                {
+                    return control.EndInvoke(result);
+                }
+            }
+
+            return null;
+        }
+
+        public static void AppendDateTimeLine(this RichTextBox target, string text)
+        {
+            if (target.InvokeRequired)
+            {
+                target.SafeInvoke(new Action(delegate
+                {
+                    target.AppendDateTimeLine(text);
+                }));
+            }
+            else
+            {
+                target.AppendText($"\n[{DateTime.Now.ToLongTimeString()}] {text}");
+                target.ScrollToCaret();
+            }
+        }
+    }
+}
